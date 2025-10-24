ea69db8539ec87c244c2997316c29aa21a884e10
Lord Ennard <andrex9400@gmail.com>
2025-10-24 09:49:55 -0500

    insertacion de usuarios

---
+++ Sinatra/db/migrations/inserts/001_insertUsers.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (name, last_name, username, password_hash, email, profile_image, birth_date, phone, is_verified) 
+VALUES 
+('Patrick', 'Leon', 'Patri', 'hash123', 'correo1@email.com', 'profile1.jpg', '1990-05-15', '+1233256789', 1),
+('Andres', 'Churampi', 'Lord_Ennard', 'hash456', 'correo2@email.com', 'profile2.jpg', '2004-01-31', '+982254321', 1),
+('Mauricio', 'Ramirez', 'MauriRamirez', 'hash789', 'correo3@email.com', 'profile3.jpg', '2002-12-10', '+554455555', 0),
+('Dylann', 'Rojas', 'Doge', 'hash323', 'correo4@email.com', 'profile4.jpg', '2004-05-15', '+17756789', 1),
+('Jose', 'Misagel', 'JoseMisagel', 'hash453', 'correo5@email.com', 'profile5.jpg', '1994-05-15', '+123459789', 1);
